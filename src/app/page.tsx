d70ae9b8cceca9342fc11713c10dfcab47249c8f
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2023-09-01 19:20:06 -0300

    feat: changing fonts and inserting header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import { Header } from '@/components/Header'
+import { Logo } from '@/components/Logo'
+
 export default function Home() {
-  return (
-    <h1>Hello</h1>
-  )
+  return <Header />
 }
