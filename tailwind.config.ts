c76bd40edd74b9af5fe667817040dcf80a6b8c34
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2023-09-01 19:18:55 -0300

    feat: adding colors in tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,11 @@
 import type { Config } from 'tailwindcss'
+import { colors } from '@self-system/tokens'
 
 const config: Config = {
-  content: [
-    './src/**/*.tsx',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-
+      colors,
     },
   },
   plugins: [],
